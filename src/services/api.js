8835279efd632906775e69cd3b97ea35099c75b8
Kong Rocxam <rocxamkong@gmail.com>
2026-04-13 19:35:41 +0300

    Create API service with axios configuration
    Set up axios instance with request and response interceptors for token management.

---
+++ src/services/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+API.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err?.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default API;
